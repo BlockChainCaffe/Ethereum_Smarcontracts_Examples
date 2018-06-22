1206694b38ad72cc3ab5432b4840e951c78ee0ca
emcy <marco.crotta@gmail.com>
2018-06-22 17:36:30 +0200

    remixed

--- Dapps/ResponsibleBox.sol
+++ Dapps/ResponsibleBox.sol
@@ -5,17 +5,18 @@ contract ResponsibleBox {
     address rbMaster;
 
     // struct of single milliactions
-    struct Source {
+    struct Scada {
         string  name;
-        unit    milliActionCounter;
+        uint    milliActionCounter;
         address adr;
     }
 
+
     // array of sources
-    Souces[] Source;
+    Scada[] Scadas;
 
     // map addresses to theyr array indexed
-    mapping (address => uint8) public positionOf;
+    mapping (address => uint) public positionOf;
 
     modifier onlyMaster {
         require(msg.sender == rbMaster);
@@ -23,43 +24,44 @@ contract ResponsibleBox {
     }
 
     // Constructor
-    funtion ResponsibleBox () public {
+    constructor () public {
         // "I'm your father"
-        rmMaster = msg.sender;
+        rbMaster = msg.sender;
     }
 
     // rmMaster can add an allowed source
-    function addSource (string _name; address _adr) onlyMaster returns (bool)  {
-            Sources.push (_name, 0, _adr);
-            positionOf[_adr] = Sources.length -1 ;
+    function addScada (string _name, address _adr) public onlyMaster returns (bool)  {
+            Scadas.push( Scada(_name, 0, _adr) );
+            positionOf[_adr] = Scadas.length -1 ;
+            return true;
     }
 
     // rmMaster can SET a value to counters
-    function setMacById (uint _id; uint _mac) onlyMaster returns (uint) w{
-        Sources[_id].milliActionCounter = _mac;
-        return Sources[_id].milliActionCounter;
+    function setMacById (uint _id, uint _mac) onlyMaster public returns (uint) {
+        Scadas[_id].milliActionCounter = _mac;
+        return Scadas[_id].milliActionCounter;
     }
 
     // rmMaster can reset a counter to a specific value
-    function setMacByAddr (address _adr; uint _mac) onlyMaster returns (uint) {
-        Sources[positionOf[_adr]] = _mac;
-        return Sources[_id].milliActionCounter;
+    function setMacByAddr (address _adr, uint _mac) onlyMaster public returns (uint) {
+        Scadas[positionOf[_adr]].milliActionCounter = _mac;
+        return Scadas[positionOf[_adr]].milliActionCounter;
     }
 
     // Increase milliActionCounter of sender
     function addMac (uint _mac) public returns (uint) {
         // Check if sender is allowed
-        pos=-1;
-        for (i=0; i<Sources.length; i++) {
-            if (Sources[i].adr == msg.sender) {
-                pos=i;
+        uint top = Scadas.length;
+        for (uint i=0; i < top; i++) {
+            if (Scadas[i].adr == msg.sender) {
                 break;
             }
-
-        require (pos >= 0);
+        }
+        // exceeded end of array?
+        require (i < top);
         // increment milliactions
-        Sources[pos].milliActionCounter += _mac;
-        return Sources[pos].milliActionCounter;
+        Scadas[i].milliActionCounter += _mac;
+        return Scadas[i].milliActionCounter;
     }
 
 }
